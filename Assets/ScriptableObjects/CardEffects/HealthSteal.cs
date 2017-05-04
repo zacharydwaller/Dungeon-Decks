ddd9bd66f10585601cc43a332637b141093110d5
Zachary Waller <zacharydwaller@gmail.com>
2017-05-03 20:52:14 -0700

    Adds several new cards

---
+++ Assets/ScriptableObjects/CardEffects/HealthSteal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Database/CardEffect/HealthSteal")]
+public class HealthSteal : CardEffect
+{
+    public int damage;
+
+    public override void DoEffect(GameObject user, Vector2 direction = default(Vector2))
+    {
+        Collider2D collider = user.GetComponent<Collider2D>();
+        RaycastHit2D rayHit;
+        Vector2 start = user.transform.position;
+        Vector2 dest = start + direction;
+
+        collider.enabled = false;
+        rayHit = Physics2D.Linecast(start, dest);
+        collider.enabled = true;
+
+        if(rayHit.transform != null)
+        {
+            Entity ent = rayHit.transform.GetComponent<Entity>();
+
+            if(ent)
+            {
+                Entity userEnt = user.GetComponent<Entity>();
+                userEnt.DoAttackAnimation(direction);
+                userEnt.health += damage;
+                ent.TakeDamage(damage);
+            }
+        }
+    }
+}
