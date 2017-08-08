b088ca4e1b76ec80b89d0d414e29da9796b42793
Zachary Waller <zacharydwaller@gmail.com>
2017-08-08 12:28:53 -0700

    Creates generic Stat Increase card effect

---
+++ Assets/ScriptableObjects/CardEffects/StatIncrease.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Database/CardEffect/StatIncrease")]
+public class StatIncrease : CardEffect
+{
+    public enum Stat
+    {
+        HP, AP, DMG, DR
+    }
+
+    public Stat stat;
+
+    public override void DoEffect(GameObject user, int amount, Vector2 direction = default(Vector2))
+    {
+        switch(stat)
+        {
+            case Stat.HP:
+                user.GetComponent<Player>().health += amount;
+                break;
+            case Stat.AP:
+                user.GetComponent<Player>().armor += amount;
+                break;
+            case Stat.DMG:
+                user.GetComponent<Player>().dmgBonus += amount;
+                break;
+            case Stat.DR:
+                user.GetComponent<Player>().dmgReduction += amount;
+                break;
+        }
+        
+    }
+}
