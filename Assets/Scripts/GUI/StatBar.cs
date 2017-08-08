477211c33a633d8eb0170c0ac82a4e3e0b79bb6c
Zachary Waller <zacharydwaller@gmail.com>
2017-08-08 11:27:50 -0700

    Adds health and AP bars

---
+++ Assets/Scripts/GUI/StatBar.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatBar : MonoBehaviour
+{
+    public enum StatType
+    {
+        Health, Armor
+    }
+
+    public Player owner;
+    public StatType statType;
+
+    private Slider slider;
+
+    private int stat
+    {
+        get
+        {
+            if(statType == StatType.Health)
+                return owner.health;
+            else
+                return owner.armor;
+        }
+    }
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if(owner == null)
+        {
+            owner = GameManager.singleton.player;
+
+            if(owner) Init();
+            else return;
+        }
+
+        int _stat = stat;
+
+        if(slider.value != _stat)
+        {
+            if(_stat > slider.maxValue)
+            {
+                slider.maxValue = _stat;
+            }
+
+            slider.value = _stat;
+        }
+    }
+
+    private void Init()
+    {
+        int _stat = stat;
+
+        slider.minValue = 0;
+        slider.maxValue = _stat;
+        slider.value = _stat;
+    }
+}
