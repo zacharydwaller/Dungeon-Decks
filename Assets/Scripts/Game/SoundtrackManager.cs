a64b7b1c7a34ae24ebe60899afaa0ca474b9c230
Zachary Waller <zacharydwaller@gmail.com>
2017-08-08 13:57:35 -0700

    Adds more music and a soundtrack manager

---
+++ Assets/Scripts/Game/SoundtrackManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackManager : MonoBehaviour
+{
+    public AudioClip[] songs;
+    public float minLength;
+
+    private int currentSong = -1;
+    private int loopsRemaining;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            if(loopsRemaining > 0)
+            {
+                loopsRemaining--;
+                audioSource.Play();
+            }
+            else
+            {
+                PlayNextSong();
+            }
+        }
+    }
+
+    private void PlayNextSong()
+    {
+        int oldSong = currentSong;
+
+        do
+        {
+            currentSong = Random.Range(0, songs.Length);
+        } while(currentSong == oldSong);
+
+        Debug.Log("Cur: " + currentSong + " Prev: " + oldSong);
+
+        loopsRemaining = GetNumLoops();
+        audioSource.clip = songs[currentSong];
+        audioSource.Play();
+    }
+
+    private int GetNumLoops()
+    {
+        AudioClip song = songs[currentSong];
+
+        return (int) (minLength / song.length);
+    }
+}
