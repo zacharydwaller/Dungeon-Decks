5c70d028ef688609f766a22612221ae136af1de7
Zachary Waller <zacharydwaller@gmail.com>
2017-09-13 14:15:06 -0700

    Added Counter Attack cards

---
+++ Assets/ScriptableObjects/AuraEffects/CounterAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Database/AuraEffect/CounterAttack")]
+public class CounterAttack : AuraEffect
+{
+    public override void OnAttacked(Entity attackedBy)
+    {
+        Vector2 dir = attackedBy.transform.position - aura.owner.transform.position;
+
+        if(dir.magnitude <= 1)
+        {
+            //aura.owner.DoAttackAnimation(dir);
+            attackedBy.TakeDamage(aura.magnitude);
+            aura.durationRemaining = 1;
+        }
+    }
+}
