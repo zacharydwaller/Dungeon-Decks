95d0c6e16fb943fca0ff53b36374a7b472ecc938
Zachary Waller <zacharydwaller@gmail.com>
2017-09-13 14:16:44 -0700

    Add Thorns cards

---
+++ Assets/ScriptableObjects/AuraEffects/Thorns.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Database/AuraEffect/Thorns")]
+public class Thorns : AuraEffect
+{
+    public override void OnAttacked(Entity attackedBy)
+    {
+        attackedBy.TakeDamage(aura.magnitude);
+    }
+}
