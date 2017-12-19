20b71e0f47029d72321ddb691b3e33d57ba15a36
Zachary Waller <zacharydwaller@gmail.com>
2017-12-19 14:02:52 -0800

    Made instruction screen work with smaller resolution

--- Assets/Scripts/GUI/MainMenu.cs
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuUI;
+    public GameObject characterCreationUI;
     public GameObject instructionsUI;
     public GameObject creditsUI;
 
+    public GameObject[] instructionText;
+    private int instructionIndex = 0;
 
     private void Start()
     {
         ShowMainMenu();
+
+        instructionIndex = 1;
+        ToggleMoreInstructions();
     }
 
     public void ShowMainMenu()
@@ -19,9 +25,21 @@ public class MainMenu : MonoBehaviour
         SwitchToPanel(mainMenuUI);
     }
 
+    public void CharacterCreationUI()
+    {
+        SwitchToPanel(characterCreationUI);
+    }
+
     public void ShowInstructions()
     {
         SwitchToPanel(instructionsUI);
+    }
+
+    public void ToggleMoreInstructions()
+    {
+        HideInstructionText();
+        instructionIndex = (instructionIndex + 1) % instructionText.Length;
+        instructionText[instructionIndex].SetActive(true);
     }
 
     public void ShowCredits()
@@ -34,6 +52,14 @@ public class MainMenu : MonoBehaviour
         mainMenuUI.SetActive(false);
         instructionsUI.SetActive(false);
         creditsUI.SetActive(false);
+    }
+
+    public void HideInstructionText()
+    {
+        foreach(var obj in instructionText)
+        {
+            obj.SetActive(false);
+        }
     }
 
     public void SwitchToPanel(GameObject panel)
